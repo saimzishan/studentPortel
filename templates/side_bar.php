4724924a34ffa0e60d751609541960b1280bb3a7
saimzishan <saimzishan@gmail.com>
2017-08-29 07:57:07 -0700

    changes in side bar

--- templates/side_bar.php
+++ templates/side_bar.php
@@ -1,3 +1,9 @@
+<?php 
+   function curPageName() {
+     return substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
+  }
+?>
+
  <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
@@ -15,28 +21,41 @@
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu">
         <li class="header">MAIN NAVIGATION</li>
-        <li class="treeview">
+      <?php $curntPage =  curPageName(); if($curntPage == "index.php") { ?>
+        <li class="treeview active"> <?php
+        } else{?>  <li class="treeview"> <?php }  ?>
           <a href="./">
             <i class="fa fa-users"></i> <span>Students</span>
           </a>
         </li>
-        <li class="treeview">
+        <?php $curntPage =  curPageName(); if($curntPage == "teachers.php") { ?>
+        <li class="treeview active"> <?php
+        } else{?>  <li class="treeview"> <?php }  ?>
           <a href="./teachers.php">
             <i class="fa fa-user-secret"></i>
-            <span>Teachers</span>
+            <span> Teachers</span>
           </a>
-        </li><li class="treeview">
+        </li>
+        <?php $curntPage =  curPageName(); if($curntPage == "subjects.php") { ?>
+        <li class="treeview active"> <?php
+        } else{?>  <li class="treeview"> <?php }  ?>
           <a href="./subjects.php">
             <i class="fa fa-suitcase"></i>
+
             <span>Subjects</span>
           </a>
-        </li><li class="treeview">
+        </li>
+        <?php $curntPage =  curPageName(); if($curntPage == "fee.php") { ?>
+        <li class="treeview active"> <?php
+        } else{?>  <li class="treeview"> <?php }  ?>
           <a href="./fee.php">
             <i class="fa fa-file"></i>
             <span>Students Fee</span>
           </a>
         </li>
-        <li class="treeview">
+        <?php $curntPage =  curPageName(); if($curntPage == "results.php") { ?>
+        <li class="treeview active"> <?php
+        } else{?>  <li class="treeview"> <?php }  ?>
           <a href="./results.php">
           <i class="fa fa-files-o"></i>
             <span>Results</span>
