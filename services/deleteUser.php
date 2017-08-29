f30637041787da8b4f4db8a24cb1cb1b29f19772
saimzishan <saimzishan@gmail.com>
2017-08-29 01:56:35 -0700

    change in deletion

--- services/deleteUser.php
+++ services/deleteUser.php
@@ -5,9 +5,11 @@ if( isset($_GET['del']) ) {
 	$userId = $_GET['del'];
 	$delUser = "DELETE FROM students where id=".$userId;
 	//$updateUser = "UPDATE students set firstname='".$firstname."', lastname='".$lastname."', type='".$type."', active='".$active."', created_at='".date("Y-m-d H:i:s")."' WHERE id=".$userId;
-	$rs = mysqli_query($conn, $delUser);
-	$resp = array("success" => "1", "msg" => "User Deleted Successfully.");
-	echo json_encode( $resp );
+	$rs = mysqli_query($dbc, $delUser);
+	if($rs) {header("Location: /studentPortel/?msg=Record Deleted successfully...");
+	} else {
+		header("Location: /studentPortel/?error=".mysqli_error($dbc));
+	}
 
 } else {
 	$resp = array("error" => "1" ,"errorMsg" => "Invalid Inputs","msg" => "Invalid Inputs");
