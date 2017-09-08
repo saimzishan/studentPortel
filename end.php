393e5936899f257c781a6d275ad9c8de2b373323
saimzishan <saimzishan@gmail.com>
2017-09-08 07:25:51 -0700

    Changes in footer

--- end.php
+++ end.php
@@ -16,7 +16,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.3.8
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
+    <strong>Copyright &copy; 2014-2016 <a>Developed by Sehzad Gull</a>.</strong> All rights
     reserved.
   </footer>
 
